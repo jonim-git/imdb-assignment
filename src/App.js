6226522f972cd02447280fad0815689518d24090
jonim-git <jonimaki1@gmail.com>
2021-12-07 01:17:53 +0200

    valmis

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState} from 'react'
+import axios from 'axios';
 
-function App() {
+const URL = 'http://localhost/imdb/'
+
+function App () {
+  const [leffat, setLeffat] = useState([]);
+  const [lyhyet, setLyhyet] = useState([]);
+
+  function etsi(e) {
+    e.preventDefault();
+    axios
+      .get(URL+ 'bean.php')
+      .then(response => {
+        setLeffat(response.data)
+        console.log(response.data)
+      })
+      .catch(error => {
+        alert(error)
+      })
+  }
+
+  function etsiLyhyet(e) {
+    e.preventDefault();
+    axios
+      .get(URL + 'lyhyetleffat.php')
+      .then(response => {
+        setLyhyet(response.data)
+        console.log(response.data)
+      })
+      .catch(error => {
+        alert(error)
+      })
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className='container-fluid'>
+      <h1 id="center">Kaikki mitä olet halunnut IMDB:ltä!</h1>
+      <div className="row">
+        <div className="col-6">
+      <br />
+      <button onClick={etsi} >Paina tästä löytääkseni parhaat Bean-leffat!</button>
+      <br />
+      <ol>
+         {leffat?.map(leffa => (
+          <li key={leffa.title_id}>
+            <b><p>{leffa.primary_title}</p></b>
+            <p>Vuosi: {leffa.start_year}</p>
+            <p>Arvosana: {leffa.average_rating}</p>
+          </li>
+        ))}  
+      </ol>
+      </div>
+      <div className="col-6">
+      <br />
+      <button onClick={etsiLyhyet} >Paina tästä löytääkseni lyhyitä action-leffoja suomenkielisillä julkaisunimillä!</button>
+      <br />
+      <ol>
+        {lyhyet?.map(lyhyt => (
+          <li key={lyhyt.title_id}>
+            <b><p>{lyhyt.primary_title}</p></b>
+            <p>Vuosi: {lyhyt.start_year}</p>
+            <p>Pituus: {lyhyt.runtime_minutes}</p>      
+          </li>
+        ))}  
+
+        </ol>
+        </div>
+        </div>
+</div>
+  )
 }
 
-export default App;
+export default App
